5574bfd8056716b12400a06ba69f723275f3688c
0rgan1cT3rm1nat0r <rmarshall2023@Elam.co.uk>
2025-02-03 10:45:19 +0000

    awd

--- Assets/UI/RecenterOrigin.cs
+++ Assets/UI/RecenterOrigin.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+/*
 public class RecenterOrigin : MonoBehaviour
 {
     public Transform head;
@@ -36,59 +36,54 @@ public class RecenterOrigin : MonoBehaviour
         }
     }
 }
+*/
 
+public class RecenterOrigin : MonoBehaviour
+{
+    public Transform head;
+    public Transform origin;
+    public Transform target;
 
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.InputSystem;
+    public InputActionProperty recenterButton;
 
-//public class RecenterOrigin : MonoBehaviour
-//{
-//    public Transform head;
-//    public Transform origin;
-//    public Transform target;
-//
-//    public InputActionProperty recenterButton;
-//
-//    public void Recenter()
-//    {
-//        if (head == null || origin == null || target == null)
-//        {
-//            Debug.LogError("One or more references are missing in the inspector.");
-//            return;
-//        }
-//
-//        Debug.Log("Recenter triggered");
-//
-//        // Calculate the offset
-//        Vector3 offset = head.position - origin.position;
-//        offset.y = 0;  // Ignore the vertical offset
-//
-//        // Move the origin to target position minus the offset
-//        origin.position = target.position - offset;
-//
-//        // Calculate the angle difference between the head and target directions
-//        Vector3 targetForward = target.forward;
-//        targetForward.y = 0;
-//        Vector3 cameraForward = head.forward;
-//        cameraForward.y = 0;
-//
-//        float angle = Vector3.SignedAngle(cameraForward, targetForward, Vector3.up);
-//
-//        // Debug the calculated angle
-//        Debug.Log("Calculated angle: " + angle);
-//
-//        // Apply the rotation to the origin
-//        origin.rotation = Quaternion.Euler(0, angle, 0);
-//    }
-//
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        if (recenterButton.action.WasPressedThisFrame())
-//        {
-//            Recenter();
-//        }
-//    }
-//}
+    public void Recenter()
+    {
+        if (head == null || origin == null || target == null)
+        {
+            Debug.LogError("One or more references are missing in the inspector.");
+            return;
+        }
+
+        Debug.Log("Recenter triggered");
+
+        // Calculate the offset
+        Vector3 offset = head.position - origin.position;
+        offset.y = 0;  // Ignore the vertical offset
+
+        // Move the origin to target position minus the offset
+        origin.position = target.position - offset;
+
+        // Calculate the angle difference between the head and target directions
+        Vector3 targetForward = target.forward;
+        targetForward.y = 0;
+        Vector3 cameraForward = head.forward;
+        cameraForward.y = 0;
+
+        float angle = Vector3.SignedAngle(cameraForward, targetForward, Vector3.up);
+
+        // Debug the calculated angle
+        Debug.Log("Calculated angle: " + angle);
+
+        // Apply the rotation to the origin
+        origin.rotation = Quaternion.Euler(0, angle, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (recenterButton.action.WasPressedThisFrame())
+        {
+            Recenter();
+        }
+    }
+}
